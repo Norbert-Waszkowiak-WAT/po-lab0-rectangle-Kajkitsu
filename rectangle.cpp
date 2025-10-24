da0e53269f6a4d66745d6367d22dc37ce52295a0
Norbert Waszkowiak <45451581+Kajkitsu@users.noreply.github.com>
2025-10-24 10:37:23 +0000

    update

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 
 class Rectangle {
+ private:
+  double width;
+  double height;
+ public:
+  Rectangle(double w, double h): width(w), height(h) {}
 
+  void setWidth(double w) {
+    width = w;
+  }
+
+  double perimeter() const {
+    return 2 * (width + height);
+  }
+
+  double getWidth() const {
+   return width;
+  }
 };
