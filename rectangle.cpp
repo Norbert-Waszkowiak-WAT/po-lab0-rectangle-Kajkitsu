e5fc937051910992e00d88d6c35d33281a71c1c6
Norbert Waszkowiak <45451581+Kajkitsu@users.noreply.github.com>
2025-11-17 10:01:28 +0000

    hgcv

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,33 +1,35 @@
 #include <iostream>
 using namespace std;
+
 class Rectangle {
 private:
     double width;
     double height;
 public:
-    Rectangle(double w, double h)
-        : width(w), height(h) {}
+    Rectangle(double w, double h):
+    width(w),height(h){};
+
     void setWidth(double w){
-        width = w;
+        width=w;
     }
     void setHeight(double h){
-        height = h;
+        height=h;
     }
-    double getWidth()const{
+    double getWidth() const{
         return width;
     }
-    double getHeight()const{
+    double getHeight() const{
         return height;
     }
-    double area() const{
-        return width * height;
+    double area()const{
+        return width*height;
     }
-    double perimeter() const{
-        return 2 * (width + height);
+    double perimeter()const{
+        return 2*(height+width);
     }
-    void display() const{
-        cout << "Rectangle: " << width << " x " << height << endl;
-        cout << "Area: " << area() << endl;
-        cout << "Perimeter: " << perimeter() << endl;
+    void display()const{
+                  cout<< "Rectangle: "<< width << " x "<<height<<endl;
+                  cout<< "Area: "<< area()<<endl;
+                  cout<< "Perimeter: "<<perimeter();
     }
 };
